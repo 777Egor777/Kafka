c26730db518ff233ba23a1b4e8a7dc3b6bb71011
777Egor777 <genevabuy@yandex.ru>
2021-04-26 11:16:37 +0300

    Add util class for creating Producer obj from Properties obj

---
+++ src/main/java/ru/yandex/cloud/kafka/train/util/ProducerUtility.java
@@ -0,0 +1,19 @@
+package ru.yandex.cloud.kafka.train.util;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+
+import java.util.Properties;
+
+/**
+ * Util class for Producer.
+ *
+ * @author Geraskin Egor(geraskin@yandex-team.ru)
+ * @since 4/25/21
+ */
+public class ProducerUtility {
+    public static <T1, T2>  Producer<T1, T2> makeProducerFromProperties(String propsFileName) {
+        Properties props = PropertiesLoader.load(propsFileName);
+        return new KafkaProducer<>(props);
+    }
+}
